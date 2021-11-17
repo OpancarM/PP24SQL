e3947afd901a896da5348b652370670330e33004
Mateo Opančar <93339980+OpancarM@users.noreply.github.com>
2021-11-17 18:40:03 +0100

    Create github.sql

---
+++ github.sql
@@ -0,0 +1,8 @@
+drop database if exists githubprimjer;
+create database githubprimjer;
+use githubprimjer;
+create table git
+(
+    sifra not null primary key auto_increment,
+    naziv varchar(50) not null;
+);
