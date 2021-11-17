fc9cc7809f5f6e4064bb73a3ab63fadf70ff7c5c
Mateo Opančar <93339980+OpancarM@users.noreply.github.com>
2021-11-17 18:42:24 +0100

    Update github.sql

--- github.sql
+++ github.sql
@@ -1,8 +1,9 @@
 drop database if exists githubprimjer;
-create database githubprimjer;
+create database githubprimjer character set utf8;
 use githubprimjer;
-create table git
+create table git;
 (
     sifra not null primary key auto_increment,
     naziv varchar(50) not null;
 );
+insert into git(sifra,naziv) values (null,'git');
