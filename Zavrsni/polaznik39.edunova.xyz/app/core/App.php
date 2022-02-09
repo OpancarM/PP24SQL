554e3ca24f191d76b1c4b81ac5590634969f7a93
Mateo Opančar <opancarmateo@gmail.com>
2022-02-09 11:11:35 +0100

    Final
    Updating final

---
+++ Zavrsni/polaznik39.edunova.xyz/app/core/App.php
@@ -0,0 +1,10 @@
+<?php
+
+class App
+{
+    public static function start()
+    {
+        $route = Request::getRoute();
+        echo $route;
+    }
+}
---
+++ Zavrsni/polaznik39.edunova.xyz/index.php
@@ -0,0 +1,34 @@
+<?php
+
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
+session_start();
+
+define('BP', __DIR__ . DIRECTORY_SEPARATOR);
+define('BP_APP', __DIR__ . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR);
+
+$path = implode(
+    PATH_SEPARATOR,
+    [
+        BP_APP . 'model',
+        BP_APP . 'controller',
+        BP_APP . 'core'
+    ]
+);
+
+set_include_path($path);
+
+spl_autoload_register(function($class){
+    $path = explode(PATH_SEPARATOR,get_include_path());
+    foreach($path as $p){
+        $file = $p . DIRECTORY_SEPARATOR . $class . '.php';
+        if(file_exists($file)){
+            include_once $file;
+            break;
+        }
+    }
+});
+
+App::start();
