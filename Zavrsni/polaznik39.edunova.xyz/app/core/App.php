e0b58ee789c356c1a6ea024de8449f796d29db74
Mateo Opančar <opancarmateo@gmail.com>
2022-02-11 16:47:34 +0100

    Update App.php
    Delete excess code

--- Zavrsni/polaznik39.edunova.xyz/app/core/App.php
+++ Zavrsni/polaznik39.edunova.xyz/app/core/App.php
@@ -5,6 +5,29 @@ class App
     public static function start()
     {
         $route = Request::getRoute();
-        echo $route;
+
+        $parts = explode('/',$route);
+
+        $class='';
+        if(!isset($parts[1]) || $parts[1]===''){
+            $class='Index';
+        }else{
+            $class=ucfirst($parts[1]);
+        }
+        $class .= 'Controller';
+
+        $method='';
+        if(!isset($parts[2]) || $parts[2]===''){
+            $method='index';
+        }else{
+            $method=$parts[2];
+        }
+
+        if(class_exists($class) && method_exists($class,$method)){
+            $instance = new $class();
+            $instance->$method();
+        }else{
+            echo $class . '->' . $method . '() do not exist';
+        }
     }
 }
