7b8f448c529bf030fafca77ba6f3ea02cdbbb068
Mateo Opančar <opancarmateo@gmail.com>
2022-02-11 16:48:07 +0100

    Create View.php
    Creating view

---
+++ Zavrsni/polaznik39.edunova.xyz/app/core/View.php
@@ -0,0 +1,20 @@
+<?php
+
+class View
+{
+    private $template;
+
+    public function __construct($template='template')
+    {
+        $this->template=$template;
+    }
+
+    public function render($phtmlPage,$parameters=[])
+    {
+        ob_start();
+        extract($parameters);
+        include_once BP_APP . 'view' . DIRECTORY_SEPARATOR . $phtmlPage. '.phtml';
+        $content = ob_get_clean();
+        include_once BP_APP . 'view' .  DIRECTORY_SEPARATOR . $this->template . '.phtml';
+    }
+}
