c82d953d6b93aa7e8a2b9e5a513605770025e265
Mateo Opančar <opancarmateo@gmail.com>
2022-02-23 12:09:14 +0100

    Update configuration.php
    add database

--- Zavrsni/polaznik39.edunova.xyz/app/configuration.php
+++ Zavrsni/polaznik39.edunova.xyz/app/configuration.php
@@ -3,5 +3,11 @@
 return [
     'dev'=>true,
     'url'=>'http://polaznik39.edunova.xyz/',
-    'titleApp'=>'John Doe Tailoring'
+    'titleApp'=>'John Doe Tailoring',
+    'data'=>[
+        'server'=>'localhost',
+        'data'=>'edunovapp24',
+        'user'=>'edunova',
+        'password'=>'edunova'
+    ]
 ];
