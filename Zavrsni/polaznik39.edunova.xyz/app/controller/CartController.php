8466416707dc5faf3ec419305ed6578d90357dd8
Mateo Opančar <opancarmateo@gmail.com>
2022-03-04 17:43:26 +0100

    Create CartController.php
    Preparation

---
+++ Zavrsni/polaznik39.edunova.xyz/app/controller/CartController.php
@@ -0,0 +1,14 @@
+<?php
+
+class CartController extends AuthorizationController
+{
+
+    private $viewDir = 
+                'private' . DIRECTORY_SEPARATOR . 
+                    'cart' . DIRECTORY_SEPARATOR;
+
+    public function index()
+    {
+       $this->view->render($this->viewDir . 'index');
+    }   
+}
