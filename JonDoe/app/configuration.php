2ecd0e7a7c3d4ca11a18e37f9e3796fa200f83ef
Mateo Opančar <opancarmateo@gmail.com>
2022-03-14 11:00:55 +0100

    Update configuration.php
    sql update

--- JonDoe/app/configuration.php
+++ JonDoe/app/configuration.php
@@ -12,3 +12,33 @@ return [
     ]
 ];
 
+
+
+if($_SERVER['SERVER_ADDR']==='127.0.0.1'){
+    $url='http://jondoe.xyz/';
+    $dev=true;
+    $data=[
+        'server'=>'localhost',
+        'data'=>'edunovapp24',
+        'user'=>'edunova',
+        'password'=>'edunova'
+    ];
+}else{
+    $url='https://polaznik39.edunova.hr/';
+    $dev=false;
+    $baza=[
+        'server'=>'localhost',
+        'data'=>'junona_edunovapp24',
+        'user'=>'junona',
+        'password'=>'$2a$12$.a4gBTmAfWqd8gDwlD1oM.hacroGWNd7t6kOxEB1MhK7dqRvYaPoS'
+    ];
+}
+
+return [
+    'dev'=>$dev,
+    'url'=>$url,
+    'titleApp'=>'John Doe Tailoring',
+    'data'=>$data
+];
+
+
