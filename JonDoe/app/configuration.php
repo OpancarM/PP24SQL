19bb6d05a628bd54967ace2cf42f83ce27625465
Mateo Opančar <opancarmateo@gmail.com>
2022-03-20 22:33:39 +0100

    Update configuration.php
    cleaning a code

--- JonDoe/app/configuration.php
+++ JonDoe/app/configuration.php
@@ -1,16 +1,6 @@
 <?php
 
-return [
-    'dev'=>true,
-    'url'=>'http://jondoe.xyz/',
-    'titleApp'=>'John Doe Tailoring',
-    'data'=>[
-        'server'=>'localhost',
-        'data'=>'edunovapp24',
-        'user'=>'edunova',
-        'password'=>'edunova'
-    ]
-];
+
 
 
 
