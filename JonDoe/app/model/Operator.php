2a1afe9480364bee64e769b2263565b1f6512e8f
Mateo Opančar <opancarmateo@gmail.com>
2022-03-20 22:34:00 +0100

    Update Operator.php
    cleaning a code

--- JonDoe/app/model/Operator.php
+++ JonDoe/app/model/Operator.php
@@ -7,30 +7,19 @@ class Login
         $connection = DB::getInstance();
 
         $query = $connection->prepare('
-            select * from user where email=:email
+        
+        select * from operator where email=:email;
+        
         ');
-        $query->execute(['email' => $email]);
-
+        $query->execute(['email'=>$email]);
         $operator = $query->fetch();
-
-        if ($operator == null) {
-            $query = $connection->prepare('
-            select * from operator where email=:email
-            ');
-            $query->execute(['email' => $email]);
-
-            $operator = $query->fetch();
-        }
-
-        if ($operator == null){
+        if($operator==null){
             return null;
         }
-
-        if (!password_verify($password, $operator->user_password)){
+        if(!password_verify($password,$operator->user_password)){
             return null;
         }
-
-        unset($operator->user_password);
+        unset($operator->lozinka);
         return $operator;
     }
 }
