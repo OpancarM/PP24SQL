9bd6c382f871879257ec154a1f8a33398ac4930c
Mateo Opančar <opancarmateo@gmail.com>
2022-04-15 16:07:55 +0200

    Authorization customer

--- JonDoeBootstrap/app/model/Operator.php
+++ JonDoeBootstrap/app/model/Operator.php
@@ -2,23 +2,47 @@
 
 class Operator
 {
+
     public static function authorization($email, $password)
     {
         $connection = DB::getInstance();
 
+        
         $query = $connection->prepare('
-        
-        select * from operator where email=:email;
-        
+        select * from operator where email=:email
         ');
-        $query->execute(['email'=>$email]);
+        $query->execute(['email' => $email]);
+
         $operator = $query->fetch();
-        if($operator==null){
+
+        if ($operator == null) {
+            $connection->beginTransaction();
+            $query = $connection->prepare('
+                select * from customer where email=:email
+            ');
+            $query->execute(['email' => $email]);
+
+            $operator = $query->fetch();
+
+            $query = $connection->prepare('
+                update customer set 
+                lastOnline =now()
+                where email=:email
+            ');
+            $query->execute(['email' => $email]);
+
+            $connection->commit();
+        }
+
+        if ($operator == null){
             return null;
         }
-        if(!password_verify($password,$operator->userpassword)){
+
+        if (!password_verify($password, $operator->userpassword)){
             return null;
         }
+
+        // Removing password from session
         unset($operator->userpassword);
         return $operator;
     }
