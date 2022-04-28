6947de6351b4115fdc89b6bfc2cf5e7419a5ad62
Mateo Opančar <opancarmateo@gmail.com>
2022-04-28 18:30:14 +0200

    Update ProductController.php

--- JonDoeBootstrap/app/controller/ProductController.php
+++ JonDoeBootstrap/app/controller/ProductController.php
@@ -15,6 +15,7 @@ class ProductController extends AuthorizationController
         /*$this->nf = new \NumberFormatter("hr-HR", \NumberFormatter::DECIMAL);
         $this->nf->setPattern('#,##0.00 kn');*/
         $this->product = new stdClass();
+        $this->product->id=0;
         $this->product->item_name='';
         $this->product->item_price='';
         $this->product->item_description='';
@@ -23,124 +24,44 @@ class ProductController extends AuthorizationController
     public function index()
     {
         $product = Product::read();
-       
-        /*foreach($products as $product){
-            $product->item_price=$this->nf->format($product->item_price);
-        }*/
 
        $this->view->render($this->viewDir . 'index',[
            'products' => $product,
        ]);
     }   
 
-    public function add()
+    public function details($id=0)
     {
-        $this->view->render($this->viewDir . 'add',[
-            'message'=>'',
-            'product'=>$this->product
-        ]);
-    }
-
-    public function change($id)
-    {
-        $this->product = Product::readOne($id);
-
-        /*if($this->product->item_price==0){
-            $this->product->item_price='';
+        if($id===0){
+            $this->view->render($this->viewDir . 'details',[
+                'product'=>$this->product,
+                'message'=>'Add data',
+                'action'=>'Add new'
+            ]);
         }else{
-            $this->product->item_price=$this->nf->format($this->product->item_price);
-        }*/
-
-        $this->view->render($this->viewDir . 'change',[
-            'message'=>'Change data',
-            'product'=>$this->product
-        ]);
-    }
-
-    public function addNew()
-    {
-        $this->prepareData();
-
-        if($this->controlName()
-        && $this->controlPrice()
-        && $this->controlDescription()){
-            Product::create((array)$this->product);
-            header('location:' . App::config('url').'product/index');
-        }else{
-            $this->view->render($this->viewDir.'add',[
-                'message'=>$this->message,
-                'product'=>$this->product
+            $this->view->render($this->viewDir . 'details',[
+                'product'=>Product::readOne($id),
+                'message'=>'Change data',
+                'action'=>'Change'
             ]);
         }
-       
+
     }
 
-    public function changeNew()
+    public function action()
     {
-        $this->prepareData();
-
-        if($this->controlName()
-        && $this->controlPrice()
-        && $this->controlDescription()){
-            Product::update((array)$this->product);
-            header('location:' . App::config('url').'product/index');
+        if($_POST['id']==0){
+            Product::create($_POST);
         }else{
-            $this->view->render($this->viewDir.'change',[
-                'message'=>$this->message,
-                'product'=>$this->product
-            ]);
+            Product::update($_POST);
         }
-       
+        header('location:' . App::config('url').'product/index');
+
     }
 
     public function delete($id)
     {
         Product::delete($id);
         header('location:' . App::config('url').'product/index');
-    }
-
-    private function controlName()
-    {
-        if(strlen($this->product->item_name)===0){
-            $this->message='Item name important!';
-            return false;
-        }
-        if(strlen($this->product->item_name)>50){
-            $this->message='Item can have only 50 letters';
-            return false;
-        }
-
-        return true;
-    }
-
-    private function controlPrice()
-    {
-        if(strlen(trim($this->product->item_price))>0){
-
-            $this->product->item_price = str_replace('.','',$this->product->item_price);
-            $this->product->item_price = (float)str_replace(',','.',$this->product->item_price);
-            if($this->product->item_price<=0){
-                $this->message='Price need to be decimal number' 
-            . $this->product->item_price;
-            $this->product->item_price='';
-            return false;
-            }
-        }
-
-        return true;
-    }
-
-    private function controlDescription()
-    {
-        if(strlen($this->product->item_description)===0){
-            $this->message='Item description important!';
-            return false;
-        }
-        if(strlen($this->product->item_description)>500){
-            $this->message='Item can have only 300 letters';
-            return false;
-        }
-
-        return true;
     }
 }
