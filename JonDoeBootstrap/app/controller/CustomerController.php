97084638182e7eb3f344bde83b37658cd44a59a2
Mateo Opančar <opancarmateo@gmail.com>
2022-04-28 18:30:10 +0200

    Update CustomerController.php

--- JonDoeBootstrap/app/controller/CustomerController.php
+++ JonDoeBootstrap/app/controller/CustomerController.php
@@ -2,16 +2,17 @@
 
 class CustomerController extends AuthorizationController
 {
-    private $viewDir =  'private' . DIRECTORY_SEPARATOR . 
-                        'customers' . DIRECTORY_SEPARATOR;
-
+    private $viewDir = 
+                'private' . DIRECTORY_SEPARATOR . 
+                    'customers' . DIRECTORY_SEPARATOR;
     private $message;
-    private $customer;
-
+    private $product;
+      
     public function __construct()
     {
         parent::__construct();
         $this->customer = new stdClass();
+        $this->customer->id=0;
         $this->customer->email='';
         $this->customer->userpassword='';
         $this->customer->firstname='';
@@ -20,132 +21,46 @@ class CustomerController extends AuthorizationController
 
     public function index()
     {
-        $customers = Customer::read();
+        $customer = Customer::read();
 
-        $this->view->render($this->viewDir . 'index', [
-            'customers' => $customers
-        ]);
-    }
+       $this->view->render($this->viewDir . 'index',[
+           'customers' => $customer,
+       ]);
+    }   
 
-    public function add()
+    public function details($id=0)
     {
-        $this->view->render($this->viewDir . 'add',[
-            'messsage'=>'',
-            'customer'=>$this->customer
-        ]);
-    }
-
-    public function change($id)
-    {
-        $this->customer = Customer::readOne($id);
-
-        $this->view->render($this->viewDir . 'change',[
-            'messsage'=>'Change the data',
-            'customer'=>$this->customer
-        ]);
-    }
-
-    public function addNew()
-    {
-        $this->prepareData();
-
-        if($this->controlEmail()
-        && $this->controlUserpassword()
-        && $this->controlFirstname()
-        && $this->controlLastname()){
-            Customer::create((array)$this->customer);
-            $this->index();
+        if($id===0){
+            $this->view->render($this->viewDir . 'details',[
+                'customer'=>$this->customer,
+                'message'=>'Add data',
+                'action'=>'Add new'
+            ]);
         }else{
-            $this->view->render($this->viewDir.'add',[
-                'message'=>$this->message,
-                'customer'=>$this->customer
+            $this->view->render($this->viewDir . 'details',[
+                'customer'=>Customer::readOne($id),
+                'message'=>'Change data',
+                'action'=>'Change'
             ]);
         }
+
     }
 
-    public function changing()
+    public function action()
     {
-        $this->prepareData();
-
-        if($this->controlEmail()
-        && $this->controlUserpassword()
-        && $this->controlFirstname()
-        && $this->controlLastname()){
-            Customer::create((array)$this->customer);
-            $this->index();
+        if($_POST['id']==0){
+            Customer::create($_POST);
         }else{
-            $this->view->render($this->viewDir.'change',[
-                'message'=>$this->message,
-                'customer'=>$this->customer
-            ]);
+            Customer::update($_POST);
         }
+        header('location:' . App::config('url').'customer/index');
+
     }
 
     public function delete($id)
     {
         Customer::delete($id);
         header('location:' . App::config('url').'customer/index');
-    }
-
-    private function prepareData()
-    {
-        $this->customer=(object)$_POST;
-    }
-
-    private function controlEmail()
-    {
-        if(strlen($this->customer->email)===0){
-            $this->message='Email Required';
-            return false;
-        }
-        if(strlen($this->customer->email)>50){
-            $this->message='Email cannot be longer than 50 characters';
-            return false;
-        }
-
-        return true;
-    }
-
-    private function controlUserpassword()
-    {
-        if(strlen($this->customer->userpassword)===0){
-            $this->message='Password Required';
-            return false;
-        }
-        if(strlen($this->customer->userpassword)>50){
-            $this->message='Password cannot be longer than 50 characters';
-            return false;
-        }
-
-        return true;
-    }
-
-    private function controlFirstname()
-    {
-        if(strlen($this->customer->firstname)===0){
-            $this->message='Firts name Required';
-            return false;
-        }
-        if(strlen($this->customer->firstname)>50){
-            $this->message='First name cannot be longer than 50 characters';
-            return false;
-        }
-
-        return true;
-    }
-
-    private function controlLastname()
-    {
-        if(strlen($this->customer->lastname)===0){
-            $this->message='Last name Required';
-            return false;
-        }
-        if(strlen($this->customer->lastname)>50){
-            $this->message='Last name cannot be longer than 50 characters';
-            return false;
-        }
-
-        return true;
     }
 }
 
