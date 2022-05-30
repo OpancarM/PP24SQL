a0514f8f1bf5b055c21e0719799e7b1fddb5ae5b
Mateo Opančar <opancarmateo@gmail.com>
2022-05-30 21:06:44 +0200

    Update CartController.php

--- JonDoeBootstrap/app/controller/CartController.php
+++ JonDoeBootstrap/app/controller/CartController.php
@@ -7,8 +7,33 @@ class CartController extends AuthorizationController
                 'private' . DIRECTORY_SEPARATOR . 
                     'cart' . DIRECTORY_SEPARATOR;
 
-    public function index()
+
+     public function index()
     {
-       $this->view->render($this->viewDir . 'index');
-    }  
+        $this->view->render($this->viewDir . 'index',[
+        'cart' => Cart::read()
+        ]);
+    } 
+
+    public function addtocart($productId , $quantity=1, $pizza=false)
+    {
+        $customerId = $_SESSION['authorized']->id;
+
+        if (Cart::getCart($customerId) === null) {
+            Cart::create($customerId);
+        }
+        $cartId = Cart::getCart($customerId)->id;
+
+
+
+        echo Cart::addtocart($productId, $cartId) ? 'OK' : 'Error';
+    }
+
+    public function removefromcart($productId)
+    {
+        $customerId = $_SESSION['authorized']->id;
+        $cartId = Cart::getCart($customerId)->id;
+
+        echo Cart::removefromcart($productId, $cartId) ? 'OK' : 'Error';
+    }
 }
